76d6b3444e47f56468ec9c8a27f1369702b55fe3
Emilyszhang <ezhang709@student.fuhsd.org>
2021-05-07 10:23:28 -0700

    GameRunner Skeleton Class

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,4 +1,20 @@
 
 public class GameRunner {
 
+	//Fields
+	
+	private MainCharacter c;
+	private Enemy e;
+	private Ground g;
+	private Dashboard dash;
+	
+	// Constructor
+	
+	// Methods
+	
+	public void runGame() {
+		
+	}
+	
+	
 }
