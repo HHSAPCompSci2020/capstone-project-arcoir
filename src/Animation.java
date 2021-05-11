a66b3196257033fede8623118f2c4ab9344b51ce
Nspaulding758 <nspaulding758@student.fuhsd.org>
2021-05-11 09:54:40 -0700

    animation

--- src/Animation.java
+++ src/Animation.java
@@ -1,24 +1,47 @@
+
+import java.util.List;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class Animation {
+	//FIELDS
+	
+	//list of all frames
+	private List<BufferedImage> frames;	
+	//what frame is currently displayed
+	private int frameIndex = 0;			
+	//specified time between frames
+	private int deltaTime = 0;			
+	//system time after the last frame update
+	private long previousTime;			
+	//CONSTRUCTOR, takes in specified time between frames
+	public Animation(int deltaTime) {		
+		this.deltaTime = deltaTime;
+		frames = new ArrayList<BufferedImage>();
+	}
 
-	private ArrayList<BufferedImage> frames;
-	
-	public Animation() {
-		
+	//***********PUBLIC METHODS***************
+
+	public void update() {	
+		if(System.currentTimeMillis() - previousTime > deltaTime) {									
+			frameIndex++;						//updates animation to next frame 
+			if(frameIndex >= frames.size()) {	//restarts animation at end
+				frameIndex = 0;
+			}
+			previousTime = System.currentTimeMillis();
+		}
 	}
-	
-	public void act() {
-		
-	}
-	
+
 	public void addFrame(BufferedImage newFrame) {
-		frames.add(newFrame);
+		frames.add(newFrame);				//adds specified frames to the animation
 	}
-	
-	public BufferedImage getCurrentFrame() {
+
+	public BufferedImage getFrame() {		//returns current frame's index in list
+		if (frames.size() > 0) {
+			return frames.get(frameIndex);
+		}
 		return null;
 	}
-	
+
+
 }
