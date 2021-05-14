4398206259df48d5223b9b541b50bc63fd68522e
Nspaulding758 <nspaulding758@student.fuhsd.org>
2021-05-14 10:10:13 -0700

    update gamescreen

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,5 +1,5 @@
 
-public class GameScreen {
+public class GameScreen extends Screen{
 
 	//Fields
 	
@@ -7,13 +7,31 @@ public class GameScreen {
 	private Enemy e;
 	private Ground g;
 	private Dashboard dash;
+	private DrawingSurface surface;
 	
 	// Constructor
-	
+	public GameScreen(DrawingSurface surface) {
+		super(800, 600);
+		this.surface = surface;
+
+		
+	}
 	// Methods
 	
-	public void runGame() {
+	public void draw() {
+		surface.pushStyle();
 		
+		surface.background(255);   // Clear the screen with a white background
+		surface.stroke(0);     // Set line drawing color to white
+		surface.noFill();
+
+//		surface.rect(x,y,30,30);
+		
+		surface.fill(0);
+		surface.text("Move: Arrow keys",10,30);
+		surface.text("Menu: Space",10,50);
+
+		surface.popStyle();
 	}
 	
 	
