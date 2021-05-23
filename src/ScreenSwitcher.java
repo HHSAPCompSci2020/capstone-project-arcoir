c6c38092cf0ee94a753a6f9fea27d00b435860f7
Emilyszhang <ezhang709@student.fuhsd.org>
2021-05-22 23:59:40 -0700

    update game over screen

--- src/ScreenSwitcher.java
+++ src/ScreenSwitcher.java
@@ -3,6 +3,7 @@ public interface ScreenSwitcher {
 	public static final int MENUSCREEN = 0;
 	public static final int DRAWINGSCREEN = 1;
 	public static final int GAMESCREEN = 2;
+	public static final int GAMEOVERSCREEN = 3;
 	
 	public void switchScreen(int i);
 }
