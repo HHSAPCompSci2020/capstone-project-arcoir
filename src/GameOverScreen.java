f87a70b2993ae19d4ba38241c6b1e60cd5a69583
Lindsay Qin <lindsaymqin@gmail.com>
2021-05-23 22:44:37 -0700

    GameOverScreen javadoc

--- src/GameOverScreen.java
+++ src/GameOverScreen.java
@@ -2,6 +2,12 @@ import processing.core.PImage;
 import java.awt.Point;
 import java.awt.Rectangle;
 
+/**
+ * The GameOverScreen class presents the display that appears when the character loses all three of its lives. 
+ * It prompts the user to exit back to the main menu in order to customize or play again.   
+ * 
+ * @author Emily Zhang
+ */
 public class GameOverScreen extends Screen {
 	
 	private DrawingSurface surface;
@@ -9,17 +15,27 @@ public class GameOverScreen extends Screen {
 	private Rectangle menu;
 	private Dashboard dash;
 	
+	/**
+	 * Constructs a GameOverScreen.
+	 * 
+	 * @param surface The PApplet to draw the GameOverScreen with.
+	 */
 	public GameOverScreen (DrawingSurface surface) {
 		this.surface = surface;
 		menu = new Rectangle(300, 270, 200, 40);
-	
 	}
 	
+	/**
+	 * Sets up the GameOverScreen.
+	 */
 	public void setup() {
 		gameOver = surface.loadImage("resources/arcoir/gameOver.gif");		
 		dash = new Dashboard(true, true, surface.loadImage("resources/dash/help/helpIcon.gif"), surface.loadImage("resources/dash/back.gif"));
 	}
 	
+	/**
+	 * Draws the GameOverScreen.
+	 */
 	public void draw () {
 		surface.image(gameOver, 0, 0);
 
@@ -37,6 +53,9 @@ public class GameOverScreen extends Screen {
 		dash.draw(surface);
 	}
 	
+	/**
+	 * Executes methods when the mouse is left clicked.
+	 */
 	public void mousePressed(Point click) {
 		if (surface.mouseButton == surface.LEFT) {
 			if (menu.contains(click.x, click.y))
