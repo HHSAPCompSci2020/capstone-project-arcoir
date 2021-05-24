a729cb769fc914857579bbbb64fb6f510f4fe316
Lindsay Qin <lindsaymqin@gmail.com>
2021-05-23 22:25:57 -0700

    ground javadoc

--- src/Ground.java
+++ src/Ground.java
@@ -1,12 +1,11 @@
-import java.awt.Image;
 import java.awt.Rectangle;
 import processing.core.PApplet;
 import processing.core.PImage;
 
 /**
+ * The Ground class enables the characters to remain along the same x-axis by a rectangle shape.
  * 
  * @author Emily Zhang
- *
  */
 public class Ground {
 	
@@ -14,20 +13,29 @@ public class Ground {
 	private PImage image;
 	private Rectangle ground;
 	
-	// Constructor
-	
+	/**
+	 * Creates a ground object.
+	 * 
+	 * @param ground The Rectangle to be used as the ground.
+	 */
 	public Ground(Rectangle ground) {
 		this.ground = ground;
 		image = null;
 	}
-	
-	// Methods 
-	
-	
+		
+	/**
+	 * 
+	 * @return the Ground object as a Rectangle.
+	 */
 	public Rectangle getRect() {
 		return ground;
 	}
 	
+	/**
+	 * Draws the Ground.
+	 * 
+	 * @param marker the PApplet to draw the Ground with.
+	 */
 	public void drawGround(PApplet marker) {
 		//marker.rect(ground.x, ground.y, ground.width, ground.height);
 		if(image != null) {
@@ -35,10 +43,19 @@ public class Ground {
 		}
 	}
 	
+	/**
+	 * Sets the ground's image.
+	 * @param i The PImage to be added.
+	 */
 	public void setImage(PImage i) {
 		image = i;
 	}
 	
+	/**
+	 * Translates the ground horizontally.
+	 * 
+	 * @param amnt The amount to translate the ground by.
+	 */
 	public void translate(int amnt) {
 		ground.setLocation(ground.x + amnt, ground.y);
 	}
